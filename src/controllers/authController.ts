664d4bbd5cb79746b9e5ff987561974008c2c9f7
Bittu-Adaired <bittu@adaired.com>
2024-07-04 17:45:35 +0530

    [UPDATE] - Authentication and JWT, Cookies edited

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -139,7 +139,7 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       maxAge: cookieMaxAge,
     });
 
