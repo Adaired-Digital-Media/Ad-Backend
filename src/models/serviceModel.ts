31d6bab2f976d5b5f9b9249b9d0ba70b9c870f91
Bkumar48 <bittukumarinfo48@gmail.com>
2024-07-09 09:29:51 +0530

    [UPDATE] - serviceModel updated

--- src/models/serviceModel.ts
+++ src/models/serviceModel.ts
@@ -1,37 +1,37 @@
-import mongoose, { Schema } from "mongoose";
-import { ServiceTypes } from "../types/serviceTypes";
+  import mongoose, { Schema } from "mongoose";
+  import { ServiceTypes } from "../types/serviceTypes";
 
-const serviceSchema = new Schema<ServiceTypes>(
-  {
-    metaTitle: { type: String, required: true },
-    metaDescription: { type: String, required: true },
-    canonicalLink: { type: String, required: true },
-    openGraphImage: { type: String },
-    robotsText: { type: String, default: "noindex, nofollow" },
-    focusKeyword: { type: String, required: true },
-    serviceName: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
-    colorScheme: { type: String, required: true },
-    parentService: {
-      type: Schema.Types.ObjectId,
-      ref: "Service",
-      default: null,
+  const serviceSchema = new Schema<ServiceTypes>(
+    {
+      metaTitle: { type: String, required: true },
+      metaDescription: { type: String, required: true },
+      canonicalLink: { type: String, required: true },
+      openGraphImage: { type: String },
+      robotsText: { type: String, default: "noindex, nofollow" },
+      focusKeyword: { type: String, required: true },
+      serviceName: { type: String, required: true },
+      slug: { type: String, required: true, unique: true },
+      colorScheme: { type: String, required: true },
+      parentService: {
+        type: Schema.Types.ObjectId,
+        ref: "Service",
+        default: null,
+      },
+      status: { type: String, enum: ["publish", "draft"], required: true },
+      childServices: {
+        type: [
+          {
+            childServiceId: { type: Schema.Types.ObjectId, ref: "Service" },
+          },
+        ],
+      },
+      bodyData: [{ type: Schema.Types.Mixed }],
     },
-    status: { type: String, enum: ["publish", "draft"], required: true },
-    childServices: {
-      type: [
-        {
-          childServiceId: { type: Schema.Types.ObjectId, ref: "Service" },
-        },
-      ],
-    },
-    bodyData: [{ type: Schema.Types.Mixed }],
-  },
-  {
-    timestamps: true,
-  }
-);
+    {
+      timestamps: true,
+    }
+  );
 
-const Service = mongoose.model<ServiceTypes>("Service", serviceSchema);
+  const Service = mongoose.model<ServiceTypes>("Service", serviceSchema);
 
-export default Service;
+  export default Service;
